e21cb7e1df2fcc1761cda68b47743d7ec244591b
Fatos Morina <fatosi.morina@gmail.com>
2017-06-29 12:31:12 +0200

    Add before action - find books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+	before_action :find_book, only: [:show, :edit, :update, :destroy]
+
 	def show
 	end
 
@@ -24,6 +26,10 @@ class BooksController < ApplicationController
 
 	def book_parameters
 		params.require(:book).permit(:title, :description, :author)
+	end
+
+	def find_book
+		@book = Book.find(params[:id])
 	end
 
 end
