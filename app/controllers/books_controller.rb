132a3a9d44dfc668fc384aa8d45d64c95dcaa598
Fatos Morina <fatosi.morina@gmail.com>
2017-06-29 11:54:41 +0200

    Implement new create and book parameters

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,19 @@
 class BooksController < ApplicationController
 	def index
-		
 	end
+
+	def new
+	  @book = Book.new	
+	end
+
+	def create
+	  @book = Book.new(book_parameters)	
+	end
+
+	private
+
+	def book_parameters
+		params.require(:book).permit(:title, :description, :author)
+	end
+
 end
