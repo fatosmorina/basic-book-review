246827dfb5f8ad18fcb942bf2fe3e1b6c1dcbea4
Fatos Morina <fatosi.morina@gmail.com>
2017-07-01 12:17:06 +0200

    Generate category books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701070947) do
+ActiveRecord::Schema.define(version: 20170701101636) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170701070947) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
