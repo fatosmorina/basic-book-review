3be13f7b9db9f3299d190224ca732fd1231ea5c5
Fatos Morina <fatosi.morina@gmail.com>
2017-07-01 12:19:11 +0200

    Add Book Category associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701101636) do
+ActiveRecord::Schema.define(version: 20170701101726) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170701101636) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
