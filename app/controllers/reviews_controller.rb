46e916245a1377a66ecb02573d0551a43e8f825b
Fatos Morina <fatosi.morina@gmail.com>
2017-07-03 20:06:10 +0200

    Implement review_parameters

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,17 @@
 class ReviewsController < ApplicationController
+	
 	def new
 		
 	end
 
 	def create
 		
+	end
+
+
+	private
+
+	def review_parameters
+	  params.require(:review).permit(:rating, :comment)		
 	end
 end
