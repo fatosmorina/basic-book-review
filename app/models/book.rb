4be80f070c488b60dc7e11c43635449552920fec
Fatos Morina <fatosi.morina@gmail.com>
2017-07-03 20:02:17 +0200

    Add association between Review, Book, User

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user	
   belongs_to :category	
-
+  has_many :reviews
+  
   has_attached_file :book_cover, styles: { book_index: "250x350>", book_show: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :book_cover, content_type: /\Aimage\/.*\z/
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703175925) do
+ActiveRecord::Schema.define(version: 20170703180049) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20170703175925) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
